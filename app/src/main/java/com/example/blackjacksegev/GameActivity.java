232bc342331211f91c5b5f03f6ec336473154adc
segev <segevdach@gmail.com>
2025-02-16 23:11:08 +0200

    added GameView

--- app/src/main/java/com/example/blackjacksegev/GameActivity.java
+++ app/src/main/java/com/example/blackjacksegev/GameActivity.java
@@ -3,15 +3,19 @@ package com.example.blackjacksegev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     private GameView gameView;
     private CardDeck deck;
     private ArrayList<Card> player;
     private ArrayList<Card> computer;
+    private Button btnStand, btnHit;
+    private LinearLayout layout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,16 @@ public class GameActivity extends AppCompatActivity {
         deck = new CardDeck(this);
         player = new ArrayList<>();
         computer = new ArrayList<>();
-        LinearLayout layout1 = findViewById(R.id.canvasView);
+        layout1 = findViewById(R.id.canvasView);
 
         fill();
         gameView = new GameView(this,player,computer);
         layout1.addView(gameView);
+
+        btnStand = findViewById(R.id.btnStand);
+        btnHit = findViewById(R.id.btnHIT);
+        btnStand.setOnClickListener(this);
+        btnHit.setOnClickListener(this);
     }
 
     private void fill() {
@@ -32,5 +41,19 @@ public class GameActivity extends AppCompatActivity {
         computer.add(deck.remover());
         player.add(deck.remover());
         computer.add(deck.remover());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnHit && player.size()<9)
+        {
+            player.add(deck.remover());
+            gameView.setPlayer(player);
+        }
+        if(v == btnStand)
+        {
+
+        }
+
     }
 }
--- app/src/main/java/com/example/blackjacksegev/GameView.java
+++ app/src/main/java/com/example/blackjacksegev/GameView.java
@@ -22,25 +22,35 @@ public class GameView extends View {
         this.computer = computer;
     }
 
+    public void setPlayer(ArrayList<Card> player) {
+        this.player = player;
+        invalidate();
+    }
+
+    public void setComputer(ArrayList<Card> computer) {
+        this.computer = computer;
+        invalidate();
+    }
+
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
         int height = canvas.getHeight();
         int width = canvas.getWidth();
         int width2 = 0;
+        int width3 = 0;
         Paint paint = new Paint();
         Bitmap cardontable;
         for (int i = 0; i < player.size(); i++) {
             cardontable = Bitmap.createScaledBitmap(player.get(i).getBitmap(), width/player.size(), width/player.size(), true);
-            canvas.drawBitmap(cardontable,width2,height/2,paint);
-            width2 = width2 + width/player.size();
+            canvas.drawBitmap(cardontable,width3,height/2,paint);
+            width3 = width3 + width/player.size();
 
         }
-        width2 = 0;
         for (int i = 0; i < computer.size(); i++) {
             cardontable = Bitmap.createScaledBitmap(computer.get(i).getBitmap(), width/computer.size(), width/computer.size(), true);
             canvas.drawBitmap(cardontable,width2,0,paint);
-            width2 = width2 + width/player.size();
+            width2 = width2 + width/computer.size();
         }
 
     }
